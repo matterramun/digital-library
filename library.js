6d18762c769b9dd392a3345148f29e83dd1cba38
matterramun <Veradux4@protonmail.com>
2024-02-10 15:57:11 -0500

    Add funct to add to table and add test books.

--- library.js
+++ library.js
@@ -1,3 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
 function Book(titleAndAuthor, pageCount, status) {
     this.titleAndAuthor = titleAndAuthor;
     this.pageCount = pageCount;
@@ -7,5 +8,35 @@ function Book(titleAndAuthor, pageCount, status) {
     };
 }
 
+const libraryTable = document.getElementById("library");
+
+function addBookToLibrary(Book) {
+    library.push(Book)
+}
+
+function addToTable(titleAndAuthor, pageCount, status){
+    var row = libraryTable.insertRow(0);
+    var titleCell = row.insertCell(0);
+    var pageCell = row.insertCell(1);
+    var statusCell = row.insertCell(2);
+    titleCell.innerHTML = titleAndAuthor;
+    pageCell.innerHTML = pageCount;
+    statusCell.innerHTML = status;
+}
+
+function addTestBooks(library){
+    for (let i = 0; i < library.length; i++) {
+        let book = library[i];
+        addToTable(book[0], book[1], book[2]);
+    }
+}
+
+let library = [];
 const theHobbit = new Book('The Hobbit by J.R.R. Tolkien', '295 pages', 'not read yet');
-console.log(theHobbit.info());
+// console.log(theHobbit.info());
+addBookToLibrary(theHobbit);
+console.table(library);
+
+
+addTestBooks(library);
+});
